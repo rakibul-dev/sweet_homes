e74088d723bf5c3a24e0e51de9de24380705d705
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-29 05:21:23 +0600

    chore: theme registry updated

--- src/components/ThemeRegistry/ThemeRegistry.jsx
+++ src/components/ThemeRegistry/ThemeRegistry.jsx
@@ -4,11 +4,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 import NextAppDirEmotionCacheProvider from "./EmotionCache";
 
 import theme from "../../theme/theme";
+
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+
+const materialTheme = materialExtendTheme();
+
 const ThemeRegistry = ({ children }) => {
   return (
     <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
+        <CssBaseline enableColorScheme />
         {children}
       </ThemeProvider>
     </NextAppDirEmotionCacheProvider>
