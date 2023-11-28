6bbd1dda4833b6006e8e926725f126937f5bf418
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-29 05:24:18 +0600

    chore: featured agents section added

--- src/components/customer/page/home/FeaturedAgents.jsx
+++ src/components/customer/page/home/FeaturedAgents.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -14,7 +15,6 @@ import {
   IconButton,
 } from "@mui/material";
 
-import DeleteIcon from "@mui/icons-material/Delete";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -43,164 +43,186 @@ const FeaturedAgents = () => {
               enim ad minim veniam.
             </Typography>
           </Box>
-          <Grid container justifyContent="center" spacing={4}>
-            <Grid item xs={6} md={3}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardContent>
-                    <Stack justifyContent="center" alignItems="center" gap={2}>
-                      <Avatar
-                        src="https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?w=996&t=st=1701161056~exp=1701161656~hmac=f255861fcf1d2b8c490e5f981e9ed38bea6504d205e5a2fa5f8d01faf1720de2"
-                        sx={{ width: 100, height: 100 }}
-                      />
-                      <Box>
-                        <Typography variant="h6" fontSize="14px" align="center">
-                          Montreal, Canada
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          fontSize="16px"
-                          fontWeight="bold"
-                          align="center"
+          <Box width="100%">
+            <Grid container justifyContent="space-between" spacing={4}>
+              <Grid item xs={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        gap={2}
+                      >
+                        <Avatar
+                          src="/images/agents/agent1.jpg"
+                          sx={{ width: 100, height: 100 }}
+                        />
+                        <Box>
+                          <Typography
+                            variant="h6"
+                            fontSize="14px"
+                            align="center"
+                          >
+                            Montreal, Canada
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            fontSize="16px"
+                            fontWeight="bold"
+                            align="center"
+                          >
+                            Harijeet M. Siller
+                          </Typography>
+                        </Box>
+                        <Stack
+                          width="100%"
+                          direction="row"
+                          justifyContent="space-evenly"
                         >
-                          Harijeet M. Siller
-                        </Typography>
-                      </Box>
-
-                      <Stack direction="row">
-                        <IconButton aria-label="delete">
-                          <FacebookIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <LinkedInIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <TwitterIcon />
-                        </IconButton>
+                          <FacebookIcon sx={{ color: "primary.main" }} />
+                          <LinkedInIcon sx={{ color: "primary.main" }} />
+                          <TwitterIcon sx={{ color: "primary.main" }} />
+                        </Stack>
                       </Stack>
-                    </Stack>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardContent>
-                    <Stack justifyContent="center" alignItems="center" gap={2}>
-                      <Avatar
-                        src="https://img.freepik.com/premium-photo/concept-people-with-young-man-yellow-background_185193-81811.jpg?w=996"
-                        sx={{ width: 100, height: 100 }}
-                      />
-                      <Box>
-                        <Typography variant="h6" fontSize="14px" align="center">
-                          Montreal, Canada
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          fontSize="16px"
-                          fontWeight="bold"
-                          align="center"
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        gap={2}
+                      >
+                        <Avatar
+                          src="/images/agents/agent2.jpg"
+                          sx={{ width: 100, height: 100 }}
+                        />
+                        <Box>
+                          <Typography
+                            variant="h6"
+                            fontSize="14px"
+                            align="center"
+                          >
+                            Montreal, Canada
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            fontSize="16px"
+                            fontWeight="bold"
+                            align="center"
+                          >
+                            Harijeet M. Siller
+                          </Typography>
+                        </Box>
+                        <Stack
+                          width="100%"
+                          direction="row"
+                          justifyContent="space-evenly"
                         >
-                          Harijeet M. Siller
-                        </Typography>
-                      </Box>
-
-                      <Stack direction="row">
-                        <IconButton aria-label="delete">
-                          <FacebookIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <LinkedInIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <TwitterIcon />
-                        </IconButton>
+                          <FacebookIcon sx={{ color: "primary.main" }} />
+                          <LinkedInIcon sx={{ color: "primary.main" }} />
+                          <TwitterIcon sx={{ color: "primary.main" }} />
+                        </Stack>
                       </Stack>
-                    </Stack>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardContent>
-                    <Stack justifyContent="center" alignItems="center" gap={2}>
-                      <Avatar
-                        src="https://img.freepik.com/free-photo/young-gardener-woman-holding-plant-giving-thumbs-up-gesture_231208-10983.jpg?w=996&t=st=1701161189~exp=1701161789~hmac=9659d2fd75c9ea375f645107c128e2ed14d2bf91fd0d5d20bf4a0891bd67ee27"
-                        sx={{ width: 100, height: 100 }}
-                      />
-                      <Box>
-                        <Typography variant="h6" fontSize="14px" align="center">
-                          Montreal, Canada
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          fontSize="16px"
-                          fontWeight="bold"
-                          align="center"
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        gap={2}
+                      >
+                        <Avatar
+                          src="/images/agents/agent3.avif"
+                          sx={{ width: 100, height: 100 }}
+                        />
+                        <Box>
+                          <Typography
+                            variant="h6"
+                            fontSize="14px"
+                            align="center"
+                          >
+                            Montreal, Canada
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            fontSize="16px"
+                            fontWeight="bold"
+                            align="center"
+                          >
+                            Harijeet M. Siller
+                          </Typography>
+                        </Box>
+                        <Stack
+                          width="100%"
+                          direction="row"
+                          justifyContent="space-evenly"
                         >
-                          Harijeet M. Siller
-                        </Typography>
-                      </Box>
-
-                      <Stack direction="row">
-                        <IconButton aria-label="delete">
-                          <FacebookIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <LinkedInIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <TwitterIcon />
-                        </IconButton>
+                          <FacebookIcon sx={{ color: "primary.main" }} />
+                          <LinkedInIcon sx={{ color: "primary.main" }} />
+                          <TwitterIcon sx={{ color: "primary.main" }} />
+                        </Stack>
                       </Stack>
-                    </Stack>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardContent>
-                    <Stack justifyContent="center" alignItems="center" gap={2}>
-                      <Avatar
-                        src="https://img.freepik.com/free-photo/enchanting-blinde-woman-trendy-knitted-sweater-expressing-happiness-indoor-portrait-charming-european-woman-standing-orange_197531-12473.jpg?w=996&t=st=1701161277~exp=1701161877~hmac=180d31e2e4cbd71273d5182842e8553677834aa178dcca339420c3481e3836c6"
-                        sx={{ width: 100, height: 100 }}
-                      />
-                      <Box>
-                        <Typography variant="h6" fontSize="14px" align="center">
-                          Montreal, Canada
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          fontSize="16px"
-                          fontWeight="bold"
-                          align="center"
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid item xs={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        gap={2}
+                      >
+                        <Avatar
+                          src="/images/agents/agent4.avif"
+                          sx={{ width: 100, height: 100 }}
+                        />
+                        <Box>
+                          <Typography
+                            variant="h6"
+                            fontSize="14px"
+                            align="center"
+                          >
+                            Montreal, Canada
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            fontSize="16px"
+                            fontWeight="bold"
+                            align="center"
+                          >
+                            Harijeet M. Siller
+                          </Typography>
+                        </Box>
+                        <Stack
+                          width="100%"
+                          direction="row"
+                          justifyContent="space-evenly"
                         >
-                          Harijeet M. Siller
-                        </Typography>
-                      </Box>
-
-                      <Stack direction="row">
-                        <IconButton aria-label="delete">
-                          <FacebookIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <LinkedInIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                          <TwitterIcon />
-                        </IconButton>
+                          <FacebookIcon sx={{ color: "primary.main" }} />
+                          <LinkedInIcon sx={{ color: "primary.main" }} />
+                          <TwitterIcon sx={{ color: "primary.main" }} />
+                        </Stack>
                       </Stack>
-                    </Stack>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Stack>
       </Container>
     </Box>
